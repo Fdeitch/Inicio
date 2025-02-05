3ab618b83ef95f0558b22b9762d70ffafbf0f78c
Fausto <faustoluzdeitch@gmail.com>
2025-02-04 23:50:32 -0300

    atualizando

--- APPWEB/app.py
+++ APPWEB/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 df_reviews = pd.read_csv("APPWEB/customer reviews.csv")
 df_top100_books = pd.read_csv("APPWEB/Top-100 Trending Books.csv")
 
