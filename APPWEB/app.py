932a098eb7f5dcac8aafaa6c8802705bf7d6a0f2
Fausto <faustoluzdeitch@gmail.com>
2025-02-04 23:02:37 -0300

    arquivos csv

--- APPWEB/app.py
+++ APPWEB/app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import pandas as pd
+
+df_reviews = pd.read_csv("APPWEB/customer reviews.csv")
+df_top100_books = pd.read_csv("APPWEB/Top-100 Trending Books.csv")
+
+df_reviews
 
 
 
 
-
-
-st.write("Olá, mundo")
