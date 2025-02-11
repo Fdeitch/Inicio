e1c7a38cdef85f4902a75c8568819210d9c3c2c7
Fausto <faustoluzdeitch@gmail.com>
2025-02-10 23:49:03 -0300

    analise de dados

--- APPWEB/app.py
+++ APPWEB/app.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 
 
 st.set_page_config(layout="wide")
 
 df_gols = pd.read_csv("APPWEB/campeonato-brasileiro-gols.csv")
 
-df_gols
+
+#df_gols.describe() # Estatísticas descritivas
+#df_gols.head(15)
+df_gols["minuto"] = pd.to_numeric(df_gols["minuto"], errors='coerce')
+above_74 = df_gols[df_gols["minuto"] > 74]
+above_74
+
+
 
 #price_max = df_gols["book price"].max()
 #price_min = df_gols["book price"].min()
